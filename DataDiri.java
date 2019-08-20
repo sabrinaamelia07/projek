c55a88042c83cac3d48fa9adeab29f12446c058e
sabrinaamelia07 <53499334+sabrinaamelia07@users.noreply.github.com>
2019-08-20 12:01:35 +0700

    Add files via upload

---
+++ DataDiri.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data.diri;
+
+/**
+ *
+ * @author PC
+ */
+public class DataDiri {
+    private static String hobby;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+        // membuat variabel
+    {String nama, alamat ;
+    int usia ;
+    double tinggi ;
+    char darah ;
+    char gender ;
+    
+    //mengisi variabel
+    nama = "sabrina";
+    alamat = "Tulungagung";
+    usia = 16;
+    tinggi = 150;
+    darah a = null;
+    gender = 'p';
+    hobby = "renang";
+    //mencetak ke layar isi variabel
+    System.out.println ("Nama: " + nama);
+    System.out.println ("Alamat: " + alamat);
+    System.out.println ("Usia: " + usia + "tahun");
+    System.out.println ("Tinggi: " + tinggi + "cm");
+    System.out.println ("hobby: " + hobby);
+    System.out.println ("darah: " + " a");
+    System.out.println ("gender: " + " p");
+    }
+}
